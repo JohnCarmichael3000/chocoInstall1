41fecfd87eb332ab36997a4b5d375fc5de0d8005
John Carmichael <66440944+JohnCarmichael3000@users.noreply.github.com>
2020-10-05 22:17:44 -0700

    Update chocoInstall1.ps1

--- chocoInstall1.ps1
+++ chocoInstall1.ps1
@@ -1,2 +1,16 @@
 #Install Chocolately https://chocolatey.org/docs/installation
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install googlechrome -y
+
+#choco install git.install -y
+#choco install vscode -y
+#choco install azure-cli -y
+#choco install powershell-core -y
+
+#choco install python3 -y
+#choco install nodejs.install -y
+#choco install sql-server-management-studio -y
+#SSRS 
+#https://docs.microsoft.com/en-us/sql/ssdt/previous-releases-of-sql-server-data-tools-ssdt-and-ssdt-bi?view=sql-server-2017#ssdt-for-visual-studio-vs-2015
+#choco install visualstudio2015community -y
