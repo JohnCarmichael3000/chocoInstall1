fc2bd069e1b50f6ccea1a51c852062d9e1a60313
John Carmichael <66440944+JohnCarmichael3000@users.noreply.github.com>
2020-10-05 22:43:40 -0700

    Create chocoInstall2.ps1

---
+++ chocoInstall2.ps1
@@ -0,0 +1,22 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install googlechrome -y
+
+#choco install git.install -y
+#choco install vscode -y
+#choco install azure-cli -y
+#choco install powershell-core -y
+
+#choco install python3 -y
+#choco install nodejs.install -y
+#choco install sql-server-management-studio -y
+#SSRS 
+#https://docs.microsoft.com/en-us/sql/ssdt/previous-releases-of-sql-server-data-tools-ssdt-and-ssdt-bi?view=sql-server-2017#ssdt-for-visual-studio-vs-2015
+#https://docs.microsoft.com/en-us/archive/blogs/sqlgardner/ssdt-vs2015-gotcha-target-server-version-new-feature
+#choco install visualstudio2015community -y
+
+#SSDT 15.8 with VS 2017? Not 15.8.1 or newer though as support removed.
+#choco install visualstudio2017community -y
+#choco install visualstudio2015community -y
+#choco install visualstudiocommunity2013 -y
