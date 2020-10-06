abfa7199877e99c54db32773cc994a7a5361fe70
John Carmichael <66440944+JohnCarmichael3000@users.noreply.github.com>
2020-10-05 20:19:24 -0700

    Update chocoInstall1.ps1

--- chocoInstall1.ps1
+++ chocoInstall1.ps1
@@ -1,1 +1,2 @@
+#Install Chocolately https://chocolatey.org/docs/installation
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
