b196412f667057b253838b1fab8c26c687b30b26
John Carmichael <66440944+JohnCarmichael3000@users.noreply.github.com>
2020-10-05 22:21:55 -0700

    Update chocoInstall1.ps1

--- chocoInstall1.ps1
+++ chocoInstall1.ps1
@@ -13,4 +13,10 @@ choco install googlechrome -y
 #choco install sql-server-management-studio -y
 #SSRS 
 #https://docs.microsoft.com/en-us/sql/ssdt/previous-releases-of-sql-server-data-tools-ssdt-and-ssdt-bi?view=sql-server-2017#ssdt-for-visual-studio-vs-2015
+#https://docs.microsoft.com/en-us/archive/blogs/sqlgardner/ssdt-vs2015-gotcha-target-server-version-new-feature
 #choco install visualstudio2015community -y
+
+#SSDT 15.8 with VS 2017? Not 15.8.1 or newer though as support removed.
+#choco install visualstudio2017community -y
+#choco install visualstudio2015community -y
+#choco install visualstudiocommunity2013 -y
