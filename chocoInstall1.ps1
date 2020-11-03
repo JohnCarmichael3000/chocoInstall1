5effb8ab2b5d2667859a8019eaae00a7c5f6e606
John Carmichael <66440944+JohnCarmichael3000@users.noreply.github.com>
2020-11-03 15:36:10 -0800

    Update chocoInstall1.ps1

--- chocoInstall1.ps1
+++ chocoInstall1.ps1
@@ -1,23 +1,3 @@
 #Install Chocolately
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-#Install Software
-choco install googlechrome -y
-
-#choco install git.install -y
-#choco install vscode -y
-#choco install azure-cli -y
-#choco install powershell-core -y
-
-#choco install python3 -y
-#choco install nodejs.install -y
-
-#SQL
-#choco install sql-server-management-studio -y
-
-#SQL SSIS
-#choco install ssis-vs2019 -y
-#https://marketplace.visualstudio.com/items?itemName=SSIS.SqlServerIntegrationServicesProjects
-
-#SQL SSRS 
-#https://marketplace.visualstudio.com/items?itemName=ProBITools.MicrosoftReportProjectsforVisualStudio
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/JohnCarmichael3000/chocoInstall1/main/chocoInstall2.ps1'))
