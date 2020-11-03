ae4d5650a157971dc2c6a48c1367ff39e131ee7d
John Carmichael <66440944+JohnCarmichael3000@users.noreply.github.com>
2020-11-03 11:31:39 -0800

    Update chocoInstall1.ps1

--- chocoInstall1.ps1
+++ chocoInstall1.ps1
@@ -11,13 +11,13 @@ choco install googlechrome -y
 
 #choco install python3 -y
 #choco install nodejs.install -y
-#choco install sql-server-management-studio -y
-#SSRS 
-#https://docs.microsoft.com/en-us/sql/ssdt/previous-releases-of-sql-server-data-tools-ssdt-and-ssdt-bi?view=sql-server-2017#ssdt-for-visual-studio-vs-2015
-#https://docs.microsoft.com/en-us/archive/blogs/sqlgardner/ssdt-vs2015-gotcha-target-server-version-new-feature
-#choco install visualstudio2015community -y
 
-#SSDT 15.8 with VS 2017? Not 15.8.1 or newer though as support removed.
-#choco install visualstudio2017community -y
-#choco install visualstudio2015community -y
-#choco install visualstudiocommunity2013 -y
+#SQL
+#choco install sql-server-management-studio -y
+
+#SQL SSIS
+#choco install ssis-vs2019 -y
+#https://marketplace.visualstudio.com/items?itemName=SSIS.SqlServerIntegrationServicesProjects
+
+#SQL SSRS 
+#https://marketplace.visualstudio.com/items?itemName=ProBITools.MicrosoftReportProjectsforVisualStudio
